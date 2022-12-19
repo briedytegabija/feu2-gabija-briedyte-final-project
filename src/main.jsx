5e8e3dd6ce59ad410898910ca56ca745813c0bc5
Gabija Briedyte <gabijabriedyte07@gmail.com>
2022-12-20 00:49:11 +0200

    register

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,16 @@ import {getAuth} from "firebase/auth";
 import './Styles/index.css';
 import App from './App';
 
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  projectId: process.env.REACT_APP_PROJECT_ID
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     // <React.StrictMode>
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -6,6 +6,51 @@ import {useHistory} from "react-router-dom";
 import toast, {Toaster} from "react-hot-toast";
 
 const Register = () => {
+    const history = useHistory();
+    const signup = async (email, password) => {
+        await createUserWithEmailAndPassword(auth, email, password)
+        await sendEmailVerification(auth.currentUser)
+    }
+
+    const [userInfo, setUserInfo] = useState({
+        email: "",
+        password: "",
+    });
+
+    const [validFields, setValidFields] = useState({
+        email: true,
+        password: true,
+    });
+
+    const handleInputChange = (event) => {
+        setUserInfo({
+            ...userInfo,
+            [event.target.name]: event.target.value
+        });
+    }
+
+    const validateAndSubmit = (event) => {
+        // regex for email validation
+        const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}])|(([a-zA-Z\-\d]+\.)+[a-zA-Z]{2,}))$/;
+        // regex for password validation, >=6 chars
+        const passwordRegex = /^.{6,}$/;
+
+        setValidFields({
+            email: emailRegex.test(userInfo.email),
+            password: passwordRegex.test(userInfo.password),
+        });
+
+        if (emailRegex.test(userInfo.email) && passwordRegex.test(userInfo.password)) {
+            signup(userInfo.email, userInfo.password).then(() => {
+                toast("Registration successful!");
+            }).catch((error) => {
+                console.log(error);
+            });
+        }
+
+        event.preventDefault();
+    }
+    
     return (
         <>
             <Navbar loggedIn={false}/>
