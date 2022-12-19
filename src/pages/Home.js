ebb97cda2696500997ea1eb927d6b5a65d16bda8
Gabija Briedyte <gabijabriedyte07@gmail.com>
2022-12-20 00:45:09 +0200

    home

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from "../Components/Navbar";
+
+const Home = () => {
+    return (
+        <>
+            <Navbar/>
+            <main>
+                <h1 className={"title"}>Home</h1>
+            </main>
+        </>
+    );
+};
+
+export default Home;
