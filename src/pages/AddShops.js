1e11d48b84bd6e929d34f61d64c70f87bd3c1bf1
Gabija Briedyte <gabijabriedyte07@gmail.com>
2022-12-20 00:55:51 +0200

    addshops

--- src/pages/AddShops.js
+++ src/pages/AddShops.js
@@ -9,6 +9,53 @@ import {db} from "../index";
 
 const AddShops = () => {
 
+    const isAuth = useAuth();
+    const notify = (message) => toast(message);
+
+    const [data, setData] = useState({});
+    const [fieldErrors, setFieldErrors] = useState({
+        shopName: "", town: "", startYear: "", imageUrl: "", description: ""
+    });
+
+
+    const handleFormChange = (event) => {
+        setData({
+            ...data, [event.target.name]: event.target.value,
+        });
+    }
+
+    const addShop = useCallback(async (name, location, est, desc, img) => {
+        await addDoc(collection(db, "shops"), {
+            shopName: name, town: location, startYear: est, description: desc, imageUrl: img
+        })
+    }, []);
+
+    const validateAndSubmit = (event) => {
+        setFieldErrors({
+            shopName: data.shopName.length >= 4 ? "" : "Shop name must be at least 4 characters long",
+            town: data.town.length >= 4 ? "" : "Town must be at least 4 characters long",
+            startYear: data.startYear >= 1970 && data.startYear <= 2022 ? "" : "Establishment must be between 1970 and 2022",
+            imageUrl: data.imageUrl.length >= 5 ? "" : "Image url must be at least 5 characters long",
+            description: data.description.length >= 6 ? "" : "Description must be at least 5 characters long",
+        });
+        const isValid = data.shopName.length >= 4 &&
+            data.town.length >= 4 &&
+            (data.startYear >= 1970 && data.startYear <= 2022) &&
+            data.imageUrl.length >= 5 &&
+            data.description.length >= 6;
+
+        if (isValid) {
+            addShop(data.shopName, data.town, data.startYear, data.description, data.imageUrl).then(() => {
+                notify("Shop added successfully!")
+            }).catch((error) => {
+                console.log(error);
+                notify("Could not add shop!")
+            });
+        }
+        event.preventDefault();
+    }
+
+
     
     return (<>
         {!isAuth && <Redirect to={'/login'}/>}
